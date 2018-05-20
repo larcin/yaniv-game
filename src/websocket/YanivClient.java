c9b3e7ef53832ce16f2d455940a31183c7ee4661
Valentin Lamatte <valentin@getbannerman.com>
2018-05-21 01:00:24 +0200

    feat(ws-poc) Add web socket dummy poc

---
+++ src/websocket/YanivClient.java
@@ -0,0 +1,51 @@
+package websocket;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class YanivClient extends WebSocketClient {
+
+    public YanivClient(URI serverUri, Draft draft) {
+        super(serverUri, draft);
+    }
+
+    public YanivClient(URI serverURI) {
+        super(serverURI);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        send("Hello, it is me. Mario :)");
+        System.out.println("new connection opened");
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("closed with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("received message: " + message);
+    }
+
+    @Override
+    public void onMessage(ByteBuffer message) {
+        System.out.println("received ByteBuffer");
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        System.err.println("an error occurred:" + ex);
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
+        WebSocketClient client = new YanivClient(new URI("ws://localhost:8887"));
+        client.connect();
+    }
+}
